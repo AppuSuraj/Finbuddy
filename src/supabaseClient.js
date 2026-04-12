36d25ab73c04c3bcffbefba38cd5a683b120e03d
Suraj Suresh <surajsan1998@gmail.com>
2026-04-12 17:52:41 +0530

    fix: production environment variables for Supabase client

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://hxeoxripczmckfevmtuw.supabase.co';
-const supabaseKey = 'sb_publishable_kYjkd39nC3K4I3io-80RZQ_1Yo0WSn2';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
